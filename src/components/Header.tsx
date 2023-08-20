068e41ac8bd6fe206f05789c40cbc4abde69953d
hieutlt <121414290+TrungHieu2411@users.noreply.github.com>
2023-08-20 07:54:42 +0700

    cập nhật bổ sung

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ function Header() {
             <div className="col-4 " style={{ marginLeft: 60 }}>
               <Link href="/home">
               <img
-                src="../image/header/Little&LittleLogo.png"
+                src="../../image/header/Little&LittleLogo.png"
                 alt=""
                 style={{ width: 175 }}
                 className=""
@@ -76,7 +76,7 @@ function Header() {
             <div className="col-3 me-5 pe-5 mt-1">
               <label className="d-flex align-items-center justify-content-center mt-3 fw-bold">
                 <img
-                  src="../image/header/phone.png"
+                  src="../../image/header/phone.png"
                   alt=""
                   className="me-2"
                   style={{ width: 20 }}
